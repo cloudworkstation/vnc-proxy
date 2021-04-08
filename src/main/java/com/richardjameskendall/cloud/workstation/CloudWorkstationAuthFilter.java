85dc7984e2443edb6061a3d049d94c2407ea20fa
Richard Kendall <richard.kendall@gmail.com>
2021-04-08 09:33:02 +0000

    auth working

--- src/main/java/com/richardjameskendall/cloud/workstation/CloudWorkstationAuthFilter.java
+++ src/main/java/com/richardjameskendall/cloud/workstation/CloudWorkstationAuthFilter.java
@@ -22,14 +22,32 @@ public class CloudWorkstationAuthFilter implements Filter {
 
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
+        
         String requestUser = httpRequest.getHeader("X-Remote-User");
-        if(requestUser.equals(System.getenv("REMOTE_USER"))) {
-            logger.info("User in header matches the expected user");
-            chain.doFilter(request, response);
+        String expectedUser = System.getenv("REMOTE_USER");
+
+        if(requestUser != null) {
+            if(expectedUser != null) {
+                if (requestUser.equals(System.getenv("REMOTE_USER"))) {
+                    logger.info("User in header matches the expected user");
+                    chain.doFilter(request, response);
+                } else {
+                    response.setContentType("text/html");
+                    PrintWriter out = response.getWriter();
+                    out.println("Access denied.");
+                }
+            } else {
+                logger.info("No REMOTE_USER environment variable is set.");
+                response.setContentType("text/html");
+                PrintWriter out = response.getWriter();
+                out.println("Access denied.");
+            }
         } else {
+            logger.info("There is no user name available in the headers");
             response.setContentType("text/html");
             PrintWriter out = response.getWriter();
             out.println("Access denied.");
         }
+
     }
 }
